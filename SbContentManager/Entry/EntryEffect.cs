b083306ee8a73e0318483c8a42f35c9342a5a9ff
gane01 <gabor.nemeth@betssongroup.com>
2022-02-18 08:19:58 +0100

    Parse json, JsonElement, JsonNode

--- SbContentManager/Entry/EntryEffect.cs
+++ SbContentManager/Entry/EntryEffect.cs
@@ -1,6 +1,7 @@
 ﻿using SbContentManager.Asset;
 using SbContentManager.Contentstack;
 using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace SbContentManager.Entry
 {
@@ -25,16 +26,31 @@ namespace SbContentManager.Entry
 			// Get copied asset pairs: { assetId:copiedAssetId }
 			var assetIdPairs = await assetEffect.Copy(entryAssetsPairs.Select(x => x.Value).Distinct(), folderId);
 			// create new entries with new assets
+			var entryIdPairs = CreateEntry(entries, languageCodes, entryAssetsPairs, assetIdPairs);
 			// publish entries
 			// publish assets
 			// return ids of created entries
 			return new List<string>();
 		}
+        private IEnumerable<JsonElement> CreateEntry(JsonElement entries, IEnumerable<string> languageCodes, IEnumerable<KeyValuePair<string, string>> entryAssetsPairs, IEnumerable<KeyValuePair<string, string>> assetIdPairs)
+        {
+			var result = new List<JsonElement>();
 
-		private IEnumerable<KeyValuePair<string, string>> GetEntryAssets(JsonElement entries) {
+			foreach (var entry in entries.GetProperty("entries").EnumerateArray()) {
+				var entryObj = JsonNode.Parse(@"{}");
+				(entryObj as JsonObject).Add("entry", JsonSerializer.Deserialize<JsonNode>(entry));
+				RemoveLanguages(languageCodes, ref entryObj);
+				UpdateAssets(entryAssetsPairs, ref entryObj);
+				result.Add(entryObj.Deserialize<JsonElement>());
+			}
+
+			return result;
+        }
+
+        private IEnumerable<KeyValuePair<string, string>> GetEntryAssets(JsonElement entries) {
 			var result = new List<KeyValuePair<string, string>>();
 
-			// Todo EnumerateArray is a disposable resource. should i use a using block?
+			// Todo EnumerateArray is a disposable resource. should it use a using block?
 			// Todo flatten this nested foreach loop
 			foreach (var entry in entries.GetProperty("entries").EnumerateArray()) 
 			{
@@ -61,6 +77,38 @@ namespace SbContentManager.Entry
             }
 
 			return result;
+		}
+
+		private void RemoveLanguages(IEnumerable<string> languageCodes, ref JsonNode entry) 
+		{
+			var removeLanguages = entry["entry"]["locales"].AsArray()
+				.Where(locale => languageCodes.Contains(locale["language_code"].ToString()))
+				.ToList();
+
+			foreach (var language in removeLanguages) {
+				entry["entry"]["locales"]!.AsArray().Remove(language);
+			}
+		}
+		private void UpdateAssets(IEnumerable<KeyValuePair<string, string>> entryAssetsPairs, ref JsonNode entry)
+		{
+			foreach (var locale in entry["entry"]["locales"].AsArray())
+			{
+				foreach (var image in locale["images"].AsArray())
+				{
+					var entryAssetsPair = entryAssetsPairs.FirstOrDefault(x => x.Key.Equals(image["image"]["uid"].ToString()));
+					if (String.IsNullOrEmpty(entryAssetsPair.Value))
+					{
+						throw new Exception($"Failed to find the copied asset uid for entry image: {entry["entry"]["uid"]}");
+					}
+					else
+					{
+						// Create JsonELement from JsonObject
+						// https://www.c-sharpcorner.com/article/new-programming-model-for-handling-json-in-net-6/
+						//image.Image = JsonSerializer.Deserialize<JsonElement>(new JsonObject { ["image"] = copiedAssetId.Value })
+						image["image"] = entryAssetsPair.Value;
+					}
+				};
+			}
 		}
 	}
 }
--- SbContentManager/Entry/EntryModel.cs
+++ SbContentManager/Entry/EntryModel.cs
@@ -1,12 +1,85 @@
-﻿namespace SbContentManager.Entry
+﻿using System.Text.Json.Serialization;
+
+namespace SbContentManager.Entry
 {
-    public class EntryModel
+    public partial class Welcome
     {
+        [JsonPropertyName("entry")]
+        public Entry Entry { get; set; }
+    }
+
+    public partial class Entry
+    {
+        [JsonPropertyName("title")]
         public string Title { get; set; }
-        public string Description { get; set; }
-        public string FileName { get; set; }
-        public string ContentType { get; set; }
-        public string Url { get; set; }
-        public string?[]? Tags { get; set; }
+
+        [JsonPropertyName("locales")]
+        public List<Locale> Locales { get; set; }
+
+        [JsonPropertyName("tags")]
+        public List<string> Tags { get; set; }
+    }
+
+    public partial class Locale
+    {
+        [JsonPropertyName("language_code")]
+        public string LanguageCode { get; set; }
+
+        [JsonPropertyName("texts")]
+        public List<Text> Texts { get; set; }
+
+        [JsonPropertyName("richtexts")]
+        public List<Richtext> Richtexts { get; set; }
+
+        [JsonPropertyName("links")]
+        public List<LinkElement> Links { get; set; }
+
+        [JsonPropertyName("images")]
+        public List<Image> Images { get; set; }
+    }
+
+    public partial class Image
+    {
+        [JsonPropertyName("key")]
+        public string Key { get; set; }
+
+        [JsonPropertyName("image")]
+        public string ImageImage { get; set; }
+    }
+
+    public partial class LinkElement
+    {
+        [JsonPropertyName("key")]
+        public string Key { get; set; }
+
+        [JsonPropertyName("link")]
+        public LinkLink Link { get; set; }
+    }
+
+    public partial class LinkLink
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("href")]
+        public Uri Href { get; set; }
+    }
+
+    public partial class Richtext
+    {
+        [JsonPropertyName("key")]
+        public string Key { get; set; }
+
+        [JsonPropertyName("richtext")]
+        public string RichtextRichtext { get; set; }
+    }
+
+    public partial class Text
+    {
+        [JsonPropertyName("key")]
+        public string Key { get; set; }
+
+        [JsonPropertyName("text")]
+        public string TextText { get; set; }
     }
 }
